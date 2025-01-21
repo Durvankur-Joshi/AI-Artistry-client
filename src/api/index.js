49e2e3fd8cc6c5d0628b31fbb107d78e57e71821
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-01-21 21:37:52 +0530

    Updated API logic in index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,26 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: `http://localhost:8080/api/`,
+// });
+
+
+// export const GetPosts = async () => await API.get("/post/");
+// export const CreatePost = async (data) => await API.post("/post/", data);
+// export const GenerateAIImage = async (data) => await API.post("/generateImage/", data);
+
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `https://server-xrg1.onrender.com/api/`,
+  baseURL: `http://localhost:8080/api/`,
 });
 
+// User-related API calls
+// In your API file
+export const SignUpUser = async (data) => await API.post("/user/SignUp", data);
+export const UserSignIn = async (data) => await API.post("/user/signIn", data);
 export const GetPosts = async () => await API.get("/post/");
 export const CreatePost = async (data) => await API.post("/post/", data);
 export const GenerateAIImage = async (data) => await API.post("/generateImage/", data);
